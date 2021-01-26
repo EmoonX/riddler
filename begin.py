c45e1e8ca6d98005af6ed3891c127d7a05686429
Gustavo Chicato <gustavo.chicato@usp.br>
2021-01-25 23:56:44 -0300

    !begin: remaining friendly message with full command usage

--- begin.py
+++ begin.py
@@ -4,9 +4,6 @@ from discord.ext import commands
 from bot import bot
 
 
-# --------------------------------------------------------------------------- #
-
-
 @bot.command()
 async def begin(ctx):
     if ctx.message.guild:
@@ -17,9 +14,7 @@ async def begin(ctx):
         await author.send(text)
         return
     
-    text = 'Hello! We\'re going to begin building an online riddle game guild.'
-    await ctx.message.author.send(text)
-
+    text = 'Hello! We\'re going to begin setting up an online riddle guild.\n\n'
     # Build list of guilds of which caller is admin
     user = ctx.message.author
     guilds = []
@@ -28,8 +23,20 @@ async def begin(ctx):
         if member.guild_permissions.administrator:
             guilds.append(guild)
 
-    text = 'These are the available guilds of which you are the admin:\n```'
+    text += 'These are the available guilds of which you are the admin ' \
+                '(not sure which ones are related to riddles, though!):\n```'
     for i, guild in enumerate(guilds):
         text += '%d) %s\n' % (i, guild.name)
-    text += '```'
+    text += '```\n'
+
+    text += 'First of all, you need a guild account to access the ' \
+            'administration web interface, exclusive to you.\n'
+    text += 'Send me the following formatted command:\n'
+    text += '```!begin guild_number guild_alias password```'
+    text += 'Where:\n' \
+            '    **guild_number**: the guild number in the list just above\n' \
+            '    **guild_alias**: the new short alias of your guild\n' \
+            '    **password**: a strong password to log in the web interface\n\n'
+    
+    text += 'Once you\'re really sure about the info, send me! I\'ll be waiting.'
     await ctx.message.author.send(text)
