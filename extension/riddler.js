068fd45362a584e256a5a74be05ed5110fd3440b
Gustavo Chicato <gustavo.chicato@usp.br>
2021-02-01 20:50:37 -0300

    [Extension] Successful URL request and response with main server

--- extension/riddler.js
+++ extension/riddler.js
@@ -1,18 +1,24 @@
+// URL to where request will be sent to
 const SERVER_URL = 'http://74.208.183.127/process/'
 
-// Get absolute path (without domain) of current page
+// Get URL of current page
 const url = document.location.href;
 
+// Request parameters
 const params = {
   method: 'POST',
   mode: 'cors',
   headers: {
-    'Content-Type': 'text/plain'
+    'Content-Type': 'text/uri-list'
   },
   body: url
 };
 
 // Open and send request to server containing URL text
 fetch(SERVER_URL, params)
-    .then(res => {console.log(res)})
+    .then(res => {
+      res.json().then(data => {
+        console.log(data.url);
+      });
+    })
     .then(error => {console.log(error)});
--- web/process.py
+++ web/process.py
@@ -1,4 +1,4 @@
-from quart import Blueprint, make_response
+from quart import Blueprint, make_response, request
 
 # Create app blueprint
 process = Blueprint('process', __name__)
@@ -6,8 +6,5 @@ process = Blueprint('process', __name__)
 
 @process.route('/process/', methods=['POST'])
 async def proc():
-    resp = await make_response('LOL')
-    #resp.headers['Access-Control-Allow-Origin'] = '*'
-    #resp.headers['Access-Control-Allow-Methods'] = 'GET, PUT, POST, DELETE, OPTIONS'
-    #resp.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With'
-    return resp
+    url = (await request.data).decode('utf-8')
+    return {'url': url}
