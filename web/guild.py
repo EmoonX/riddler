e4bb8060dcbea28d7e4020500ddf70c052d58e25
Gustavo Chicato <gustavo.chicato@usp.br>
2021-02-02 17:08:22 -0300

    [Web UI] Updated form for insertion of secret levels

--- web/guild.py
+++ web/guild.py
@@ -19,45 +19,41 @@ async def config(alias: str):
     # Fetch guild levels info from database
     query = 'SELECT * FROM levels WHERE guild = :guild'
     levels = await database.fetch_all(query, {'guild': alias})
+    query = 'SELECT * FROM secret_levels WHERE guild = :guild'
+    secret_levels = await database.fetch_all(query, {'guild': alias})
     
-    def r(msg, new_id='', new_filename=''):
+    def r(msg):
         '''Render page and get filename cookies locally.'''
-        paths = {}
-        s = 'path_%s_' % alias
-        for name, value in request.cookies.items():
-            print(name, value)
-            if s in name:
-                id = name.replace(s, '')
-                paths[id] = value
-        if new_id:
-            # Add new inserted level on paths dict on POST request
-            paths[new_id] = new_filename
-        print(paths)
         return render_template('guild.htm', 
-                alias=alias, levels=levels, msg=msg, paths=paths)
+                alias=alias, levels=levels,
+                secret_levels=secret_levels, msg=msg)
 
     # Render page normally on GET
     if request.method == 'GET':
         return await r('')
 
-    # Insert new level info on database
+    # Insert new level and secret_level info on database
     form = await request.form
     query = 'INSERT IGNORE INTO levels VALUES ' \
             '(:guild, :category, :level_id, :path)'
-    values = {'guild': alias, 'category': 'Levels',
+    levels_values = {'guild': alias, 'category': 'Levels',
             'level_id': form['new_id'], 'path': form['new_path']}
-    await database.execute(query, values)
+    if '' not in levels_values.values():
+        await database.execute(query, levels_values)
+    query = 'INSERT IGNORE INTO secret_levels VALUES ' \
+            '(:guild, :category, :level_id, :path)'
+    secret_levels_values = {'guild': alias, 'category': 'Levels',
+            'level_id': form['new_secret_id'], 'path': form['new_secret_path']}
+    if '' not in secret_levels_values.values():
+        await database.execute(query, secret_levels_values)
 
     # Update Discord guild channels and roles with new levels info.
     # This is done by sending an request to the bot's IPC server
-    levels.append(values)
+    levels.append(levels_values)
+    secret_levels.append(secret_levels_values)
     await web_ipc.request('update',
-            guild_id=session['id'], levels=values)
+            guild_id=session['id'],
+            levels=levels_values, secret_levels=secret_levels_values)
 
-    # Save cookie for locally setting level filenames and render page
-    resp = await make_response(
-            await r('Guild info updated successfully!',
-                values['level_id'], values['path']))
-    name = 'path_%s_%s' % (alias, values['level_id'])
-    resp.set_cookie(name, values['path'])
-    return resp
+    return r('Guild info updated successfully!')
+
