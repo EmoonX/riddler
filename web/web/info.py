87ff3ee89208db98fcde0740e8e53d7eb39c0ca0
Emoon <gustavo.chicato@usp.br>
2021-08-23 18:53:01 -0300

    [Web] Add missing extension icons (bmp, pdf, jpeg)

--- web/web/info.py
+++ web/web/info.py
@@ -16,15 +16,17 @@ async def thedude():
     from process import process_url
     from util.db import database
     query = 'SELECT * FROM levels ' \
-            'WHERE riddle = "cipher" AND `index` >= 40 AND `index` <= 65 AND is_secret IS FALSE'
+            'WHERE riddle = "cipher" AND `index` >= 64 AND `index` <= 65 AND is_secret IS FALSE'
     levels = await database.fetch_all(query)
     for level in levels:
         front_path = 'http://gamemastertips.com/cipher' + level['path']
+        answer_path = 'http://gamemastertips.com/cipher' + level['answer']
         image_path = front_path.rsplit('/', maxsplit=1)[0] + '/' + level['image']
-        print(front_path, image_path)
+        from time import sleep
         await process_url(username, disc, front_path)
         await process_url(username, disc, image_path)
-        from asyncio import sleep
-        await sleep(2)
+        sleep(2)
+        # await process_url(username, disc, answer_path)
+        # sleep(2)
     
     return 'SUCCESS!', 200
