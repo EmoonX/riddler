f16f666b9bb1a215294cd31b71af479fed591440
Emoon <gustavo.chicato@usp.br>
2021-09-20 20:13:02 -0300

    [Bot] `/balthify` now working (again)

--- bot/commands/extra.py
+++ bot/commands/extra.py
@@ -15,33 +15,23 @@ class Extra(commands.Cog):
         await ctx.respond()
         await ctx.send('Pong! (%dms)' % latency)
 
-    @commands.command()
-    async def balthify(self, ctx):
+    @cog_ext.cog_slash(name='balthify')
+    async def balthify(self, ctx: SlashContext, text: str):
         '''Turn text into Balthazar-speak!'''
         
-        text = ctx.message.content.split()
-        if len(text) == 1:
-            # Command usage
-            text = '> `!balthify` - Turn text into Balthazar-speak\n' \
-                    '> • Usage: `!balthify <text>`'
-        else:
-            # Transform text into uppercase, remove spaces
-            # and punctuation and keep numbers
-            text = list(''.join(text[1:]))
-            for i in range((len(text))):
-                if text[i].isalpha():
-                    text[i] = text[i].upper()
-                elif not text[i].isdigit():
-                    text[i] = ''
-            text = ''.join(text)
+        # Transform text into uppercase, remove spaces
+        # and punctuation and keep numbers
+        text = list(text)
+        for i in range((len(text))):
+            if text[i].isalpha():
+                text[i] = text[i].upper()
+            elif not text[i].isdigit():
+                text[i] = ''
+        text = ''.join(text)
 
         # Send message
-        if text:
-            if not ctx.guild:
-                await ctx.author.send(text)
-            else:
-                await ctx.channel.send(text)
-
+        await ctx.respond()
+        await ctx.send(text)
 
 def setup(bot: commands.Bot):
     '''Add cog every time extension (module) is (re)loaded.'''
