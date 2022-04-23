d691769f03fca044874595890a62da7f8477365e
EmoonX <gustavo.chicato@usp.br>
2022-04-23 01:27:42 -0300

    [Bot/Decipher] Add `ascii_to_text` command

--- bot/commands/decipher.py
+++ bot/commands/decipher.py
@@ -14,7 +14,7 @@ class Decipher(commands.Cog):
         self.bot = bot
 
     @cog_ext.cog_slash(
-        name='bintoascii',
+        name='binary_to_text',
         options=[create_option(
             name='binary_code',
             description=(
@@ -24,7 +24,7 @@ class Decipher(commands.Cog):
             required=True,
         )],
     )
-    async def bintoascii(self, ctx: SlashContext, binary_code: str):
+    async def binary_to_text(self, ctx: SlashContext, binary_code: str):
         '''Convert binary string(s) to ASCII character representation.'''
 
         # Ignore anything that isn't '0' or '1'
@@ -33,16 +33,35 @@ class Decipher(commands.Cog):
         # Build list of converted chars from binary groups of 8
         ascii_str = []
         for k in range(0, len(binary_code) - 7, 8):
-            bin = binary_code[k:k+8]
-            n = int(bin, 2)
+            binary = binary_code[k:k+8]
+            n = int(binary, 2)
             c = chr(n)
             ascii_str.append(c)
 
         # Get resulting string (possibly blank) and send it
-        ascii_str = ''.join(ascii_str)
-        if ascii_str.isspace():
-            ascii_str = '_This message was intentionally left blank._'
-        await ctx.send(ascii_str)
+        text = ''.join(ascii_str)
+        if text.isspace():
+            text = '_This message was intentionally left blank._'
+        await ctx.send(text)
+
+    @cog_ext.cog_slash(
+        name='ascii_to_text',
+        options=[create_option(
+            name='ascii_codes',
+            description=(
+                'List of space-separated ASCII decimal codes.'
+            ),
+            option_type=OptType.from_type(str),
+            required=True,
+        )],
+    )
+    async def ascii_to_text(self, ctx: SlashContext, ascii_codes: str):
+        '''Convert ASCII codes to text.'''
+        ascii_list = ascii_codes.split()
+        text = ''.join(chr(int(k)) for k in ascii_list)
+        if text.isspace():
+            text = '_This message was intentionally left blank._'
+        await ctx.send(text)
 
 
 def setup(bot: commands.Bot):
