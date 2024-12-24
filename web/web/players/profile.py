8eb3b57cccef98dc20594309eaee402594e4215e
EmoonX <emoon@emoon.dev>
2024-12-24 17:19:47 -0300

    Fix logged user avatar not appearing

--- web/web/players/profile.py
+++ web/web/players/profile.py
@@ -1,9 +1,12 @@
 from quart import Blueprint, render_template
 
+from auth import discortd
+
 # Create app blueprint
 profile = Blueprint('profile', __name__)
 
 
 @profile.route('/user/<username>')
 async def player_profile(username: str):
-    return await render_template('players/profile.htm', username=username)
+    user = await 
+    return await render_template('players/profile.htm', user=user)
