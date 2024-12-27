896426056257f5d714c2a537801f822356f6a3df
EmoonX <emoon@emoon.dev>
2024-12-27 15:44:34 -0300

    Use `gunicorn` as HTTP server w/ underlying `uvicorn`

--- run_web.sh
+++ run_web.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+sudo killall gunicorn
 source venv/bin/activate
 source .env
+
 pushd ~/riddler/web/web
-sudo ../../venv/bin/uvicorn main:app    \
-    --host 0.0.0.0                      \
-    --port 443                          \
-    --ssl-certfile "$SSL_CERT"          \
-    --ssl-keyfile "$SSL_KEY"            \
-    --use-colors                        \
-    --workers 4
+
+current_date=$(date +%Y-%m-%d)
+sudo ../../venv/bin/gunicorn main:app               \
+    --bind 0.0.0.0:443                              \
+    --certfile "$SSL_CERT"                          \
+    --keyfile "$SSL_KEY"                            \
+    --workers 4                                     \
+    --worker-class uvicorn.workers.UvicornWorker    \
+    --access-logfile "-"                            \
+    | tee -a "$HOME/logs/web-$current_date.log"
+
 popd
