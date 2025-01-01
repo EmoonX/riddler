1261d9c77c8df09cc106477d929a8f26840a9cb6
EmoonX <emoon@emoon.dev>
2025-01-01 17:24:41 -0300

    chore: switch to `hypercorn` (hoping to be the last)

--- run_web.sh
+++ run_web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo killall gunicorn
+sudo killall hypercorn
 source venv/bin/activate
 source .env
 
@@ -8,13 +8,13 @@ pushd ~/riddler/web/web
 
 current_date=$(date +%Y-%m-%d)
 log_file="$HOME/logs/web-$current_date.log"
-nohup sudo ../../venv/bin/gunicorn main:app         \
-    --bind 0.0.0.0:443                              \
-    --certfile "$SSL_CERT"                          \
-    --keyfile "$SSL_KEY"                            \
-    --workers 4                                     \
-    --worker-class uvicorn.workers.UvicornWorker    \
-    --access-logfile "-"                            \
+nohup sudo ../../venv/bin/hypercorn     \
+    main:app                            \
+    --bind 0.0.0.0:443                  \
+    --certfile "$SSL_CERT"              \
+    --keyfile "$SSL_KEY"                \
+    --workers 4                         \
+    --access-logfile "-"                \
     &>> $log_file &
 sleep .5
 tail -f $log_file
