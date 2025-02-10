6786fcc4463009fecc81600c3d30bed68593800e
EmoonX <emoon@emoon.dev>
2025-02-10 10:11:31 -0300

    chore: update run scripts (w/ `uvicorn` back)

--- run_bot.sh
+++ run_bot.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-killall python3.12
-source venv/bin/activate
+killall python3
+source ~/riddler/venv/bin/activate
 
 pushd ~/riddler/bot
 
 current_date=$(date +%Y-%m-%d)
 log_file="$HOME/logs/bot-$current_date.log"
-nohup python3.12 . &>> "$log_file" &
+nohup python3 . &>> "$log_file" &
 sleep .5
 tail -f $log_file
 
--- run_web.sh
+++ run_web.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-sudo killall hypercorn
-source venv/bin/activate
-source .env
+sudo killall uvicorn
+source ~/riddler/.env
 
 pushd ~/riddler/web/web
 
 current_date=$(date +%Y-%m-%d)
 log_file="$HOME/logs/web-$current_date.log"
-nohup sudo ../../venv/bin/hypercorn     \
-    main:app                            \
-    --bind 0.0.0.0:443                  \
-    --certfile "$SSL_CERT"              \
-    --keyfile "$SSL_KEY"                \
-    --workers 4                         \
-    --access-logfile "-"                \
-    &>> $log_file &
+sudo nohup ../../venv/bin/uvicorn   \
+    main:app                        \
+    --host 0.0.0.0                  \
+    --port 443                      \
+    --ssl-certfile "$SSL_CERT"      \
+    --ssl-keyfile "$SSL_KEY"        \
+    --workers 2                     \
+    --access-log                    \
+    --use-colors                    \
+    &>> $log_file                   \
+    &
 sleep .5
 tail -f $log_file
 
