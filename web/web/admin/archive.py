c06a4427a56e62603a4883b3b2f1217460bff46d
EmoonX <emoon@emoon.dev>
2025-03-30 14:54:13 -0300

    web.admin.archive: implement timeline by renaming content-changed files

--- web/web/admin/archive.py
+++ web/web/admin/archive.py
@@ -35,17 +35,32 @@ class ArchivePage:
         return True
 
     def save(self):
-        path = self.path.replace('..', '(parent)')
-        full_path = f"../archive/{self.alias}{path}"
-        dir_path = os.path.dirname(full_path)
-        Path(dir_path).mkdir(parents=True, exist_ok=True)
+        base_path = self.path.replace('..', '(parent)')
+        full_path = Path(f"../archive/{self.alias}{base_path}")
+        if full_path.is_file():
+            # If the currently archived file is indeed a different one,
+            # rename it as hidden and concatenate its datetime
+            with open(full_path, 'rb') as old_file:
+                old_content = old_file.read()
+                old_hash = hashlib.md5(old_content).hexdigest()
+                if old_hash != self.content_hash:
+                    old_mtime = (
+                        datetime.fromtimestamp(full_path.stat().st_mtime)
+                        .strftime("%Y-%m-%d")
+                    )
+                    renamed_filename = f".{full_path.name}-{old_mtime}"
+                    renamed_path = f"{full_path.parent}/{renamed_filename}"
+                    os.rename(full_path, renamed_path)
+
+        # Recursively create directories (if needed) and save new archived file
+        Path(full_path.parent).mkdir(parents=True, exist_ok=True)
         with open(full_path, 'wb') as file:
             file.write(self.content)
             tnow = datetime.utcnow()
             print(
                 f"> \033[1m[{self.alias}]\033[0m "
-                f"Saved new content from \033[1;3m{self.path}\033[0m "
-                f"with hash \033[1m{self.content_hash}\033[0m "
-                f"({tnow})",
+                    f"Saved new content from \033[1;3m{self.path}\033[0m "
+                    f"with hash \033[1m{self.content_hash}\033[0m "
+                    f"({tnow})",
                 flush=True
             )
