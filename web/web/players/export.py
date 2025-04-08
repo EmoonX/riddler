bd5ae5a9de5f6f47d03514fdf754930bedf11a82
EmoonX <emoon@emoon.dev>
2025-04-07 22:54:20 -0300

    web.players.export: DB-log export time and handy counters

--- web/web/players/export.py
+++ web/web/players/export.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
 import csv
 from datetime import datetime
 import json
+from types import SimpleNamespace
 from typing import Self
 
 from quart import Blueprint, Response, request, send_file
@@ -8,47 +10,78 @@ from quart import Blueprint, Response, request, send_file
 from auth import discord, User
 from inject import get_riddles
 from levels import get_pages, absolute_paths
+from util.db import database
 from util.levels import get_ordered_levels
 
 export = Blueprint('players_export', __name__)
 
 
 class Export:
+    '''Userdata export procedures.'''
 
     def __init__(self, user: User):
-        tnow = datetime.utcnow().strftime("%Y%m%d%H%M%S")
-        self.filename = f"userdata-{user.name}-{tnow}"
-        self.data = {}
+        '''Init raw `Export` object with empty data and counters.'''
+        self.user = user
+        self.export_time = datetime.utcnow()
+        self.filename = (
+            f"userdata-{self.user.name}-" +
+            self.export_time.strftime("%Y%m%d%H%M%S")
+        )
+        self.data = defaultdict(dict)
+        self.counters = SimpleNamespace(riddles=0, levels=0, pages=0)
 
     async def build_data(self):
+        '''Build userdata dict from user-unlocked levels and pages.'''
         riddles = await get_riddles(unlisted=True)
         for riddle in riddles:
-            page_tree = await get_pages(riddle['alias'], as_json=False)
+            alias = riddle['alias']
+            page_tree = await get_pages(alias, as_json=False)
             if not page_tree:
                 continue
-            self.data[riddle['alias']] = {}
-            for level_name, level_data in page_tree.items():
-                pages = []
-                paths = absolute_paths(level_data['/'])
-                for path, page_node in sorted(paths):
-                    pages.append({
-                        'path': path,
-                        'access_time': page_node['access_time'],
-                    })
+            self.counters.riddles += 1
+            for level_name, level_node in page_tree.items():
+                level_data = self._build_level_data(alias, level_node)
+                self.data[alias][level_name] = level_data
+                if level_data.get('answer'):
+                   self.counters.levels += 1 
+                self.counters.pages += len(level_data)
+    
+    def _build_level_data(self, alias: str, level_node: dict) -> dict:
+        '''Build level-specific userdata.'''
 
-                level = {'pages': pages}
-                if 'frontPage' in level_data:
-                    level |= {'front_page': level_data['frontPage']}
-                    if 'answer' in level_data:
-                        level |= {'answer': level_data['answer']}
-                                    
-                level |= {'pages_found': level_data['/']['filesFound']}
-                if {'front_page', 'answer'}.issubset(level.keys()):
-                    level |= {'pages_total': level_data['/']['filesTotal']}
+        paths = absolute_paths(level_node['/'])
+        pages = [
+            {'path': path, 'access_time': page_node['access_time']}
+            for path, page_node in sorted(paths)
+        ]
+        level_data = {'pages': pages}
+        if front_page := level_node.get('frontPage'):
+            level_data |= {'frontPage': front_page}
+            if answer := level_node.get('answer'):
+                level_data |= {'answer': answer}
+        level_data |= {'filesFound': level_node['/']['filesFound']}
+        if {'frontPage', 'answer'}.issubset(level_data.keys()):
+            level_data |= {'filesTotal': level_node['/']['filesTotal']}
 
-                self.data[riddle['alias']][level_name] = level
+        return level_data
+
+    async def record(self):
+        '''Record user's export time and riddle progress counters.'''
+        values = {
+            'username': self.user.name,
+            'export_time': self.export_time,
+            **{f"{k[:-1]}_count": v for k, v in vars(self.counters).items()},
+        }
+        query = f"""
+            INSERT INTO _user_exports
+            ({', '.join(values.keys())})
+            VALUES ({', '.join(f":{k}" for k in values.keys())})
+        """
+        await database.execute(query, values)
 
     async def download(self, format: str) -> Response:
+        '''Generate export file and return it as attachment in response.'''
+
         format = format.lower()
         self.filename += f".{format}"
         match format:
@@ -68,13 +101,19 @@ class Export:
         return response
 
     async def _to_json(self) -> str:
+        '''Simple JSON pretty-printed dump.'''
         return json.dumps(self.data, indent=2)
     
 
-@export.route('/account/export-riddle-data')
-async def export_user_riddle_data():
+@export.route('/account/export-userdata')
+async def export_userdata():
+    '''
+    Retrieve user's current riddle data and export it
+    as a downloadable file in the given (suitable) format.
+    '''
     format = request.args.get('format', '')
     user = await discord.get_user()
     export = Export(user)
     await export.build_data()
+    await export.record()
     return await export.download(format)
