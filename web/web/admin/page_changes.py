e9edeec61d9704fafdc6c94c963c08041eeca198
EmoonX <emoon@emoon.dev>
2025-05-11 16:33:20 -0300

    web.admin.page_changes: add wildcard and tokens support

--- web/web/admin/page_changes.py
+++ web/web/admin/page_changes.py
@@ -1,4 +1,7 @@
-from pymysql.err import IntegrityError
+from copy import copy
+from itertools import chain
+import re
+
 from quart import Blueprint
 from quartcord import requires_authorization
 
@@ -16,8 +19,70 @@ async def apply_page_changes(alias: str):
     # Check for admin permissions
     await admin_auth(alias)
 
+    query = '''
+        SELECT * FROM _page_changes
+        WHERE riddle = :riddle
+    '''
+    page_changes = await database.fetch_all(query, {'riddle': alias})
+    for page_change in map(dict, page_changes):
+
+        if {'*', '?'} & set(wildcard_path := page_change['path']):
+            query = '''
+                SELECT * FROM level_pages
+                WHERE riddle = :riddle AND path LIKE :wildcard_path
+            '''
+            values = {
+                'riddle': alias,
+                'wildcard_path':
+                    wildcard_path.replace('*', '%').replace('?', '_'),
+            }
+            pages = await database.fetch_all(query, values)
+            for path in [page['path'] for page in pages]:
+                single_change = copy(page_change)
+                single_change['path'] = path
+                pattern = page_change['path'] \
+                    .replace('?', '(.)').replace('*', '(.*)')
+                t1 = re.split('[*]|[?]', page_change['new_path'])
+                t2 = list(map(lambda k: f"\{k}", range(1, len(t1))))
+                repl = ''.join(chain(*zip(t1, t2), t1[-1]))
+                single_change['new_path'] = re.sub(pattern, repl, path)
+                await _apply_change(single_change)
+
+        elif '{' in page_change['path']:
+            base_path, new_base_path = \
+                page_change['path'], page_change['new_path']
+            tokens_str = re.search('{(.+)}', base_path).group(1)
+            new_tokens_str = re.search('{(.+)}', new_base_path).group(1)
+            tokens = list(filter(None, tokens_str.split(',')))
+            new_tokens = list(filter(None, new_tokens_str.split(',')))
+            for i in range(max(len(tokens), len(new_tokens))):
+                single_change = copy(page_change)
+                single_change['path'] = (
+                    f"{base_path.replace(tokens_str, tokens[i])}"
+                    if tokens[i:] else ''
+                )
+                single_change['new_path'] = (
+                    f"{base_path.replace(new_tokens_str, new_tokens[i])}"
+                    if new_tokens[i:] else ''
+                )
+                await _apply_change(single_change)
+
+        else:
+            await _apply_change(page_change)
+
+    return 'SUCCESS :)', 200
+
+
+async def _apply_change(page_change: dict, dry_run: bool = False):
+
+    alias = page_change['riddle']
+    path, new_path = page_change['path'], page_change['new_path']
+    level_name = page_change['level']
+
     async def _update_page_level(path: str, level_name: str | None):
         '''Update level for page with given path.'''
+        if dry_run:
+            return
         query = '''
             UPDATE level_pages
             SET level_name = :level_name
@@ -32,61 +97,50 @@ async def apply_page_changes(alias: str):
             msg += f" ({level_name})"
         print(f"> \033[1m[{alias}]\033[0m {msg}")
 
-    query = '''
-        SELECT * FROM _page_changes
-        WHERE riddle = :riddle
-    '''
-    page_changes = await database.fetch_all(query, {'riddle': alias})
-    for page_change in page_changes:
-        path, new_path = page_change['path'], page_change['new_path']
-        level_name = page_change['level']
-
-        if page_change['trivial_move']:
-            # "Trivial move" means the path change was essentially
-            # due to logistics and not actually new/different content;
-            # therefore, just rename the old page and let players keep it
-            query = '''
-                UPDATE level_pages (
-                SET path = :new_path
-                WHERE riddle = :riddle AND path = :path
-            '''
-            values = {'riddle': alias,'path': path, 'new_path': new_path}
+    if page_change['trivial_move']:
+        # "Trivial move" means the path change was essentially
+        # due to logistics and not actually new/different content;
+        # therefore, just rename the old page and let players keep it
+        query = '''
+            UPDATE level_pages (
+            SET path = :new_path
+            WHERE riddle = :riddle AND path = :path
+        '''
+        values = {'riddle': alias,'path': path, 'new_path': new_path}
+        _log(
+            f"Moving page "
+            f"\033[3m{path}\033[0m -> \033[1;3m{new_path}\033[0m "
+            f"(trivial)"
+        )
+    else:
+        if path:
+            # Erase level from the old page
+            await _update_page_level(path, None)
+            if not new_path:
+                _log(f"Removing page \033[3m{path}\033[0m")
+        if new_path:
+            # Write level to the new page
+            await _update_page_level(new_path, level_name)
+            if not path:
+                _log(f"Adding page \033[1;3m{new_path}\033[0m")
+        if path and new_path:
             _log(
                 f"Moving page "
-                f"\033[3m{path}\033[0m -> \033[1;3m{new_path}\033[0m "
-                f"(trivial)"
+                f"\033[3m{path}\033[0m -> \033[1;3m{new_path}\033[0m"
             )
-        else:
-            if path:
-                # Erase level from the old page
-                await _update_page_level(path, None)
-                if not new_path:
-                    _log(f"Removing page \033[3m{path}\033[0m")
-            if new_path:
-                # Write level to the new page
-                await _update_page_level(new_path, level_name)
-                if not path:
-                    _log(f"Adding page \033[1;3m{new_path}\033[0m")
-            if path and new_path:
-                _log(
-                    f"Moving page "
-                    f"\033[3m{path}\033[0m -> \033[1;3m{new_path}\033[0m"
-                )
 
-        if path and new_path:
-            # Update level data in case the old path was a front/answer one
-            query = '''
-                UPDATE levels
-                SET path = :new_path
-                WHERE riddle = :riddle AND path = :path
-            '''
-            values = {'riddle': alias, 'path': path, 'new_path': new_path}
-            await database.execute(query, values)
-            query = '''
-                UPDATE levels
-                SET answer = :new_path
-                WHERE riddle = :riddle AND answer = :path
-            '''
-            await database.execute(query, values)
-    
-    return 'SUCCESS :)', 200
+    if path and new_path and not dry_run:
+        # Update level data in case the old path was a front/answer one
+        query = '''
+            UPDATE levels
+            SET path = :new_path
+            WHERE riddle = :riddle AND path = :path
+        '''
+        values = {'riddle': alias, 'path': path, 'new_path': new_path}
+        await database.execute(query, values)
+        query = '''
+            UPDATE levels
+            SET answer = :new_path
+            WHERE riddle = :riddle AND answer = :path
+        '''
+        await database.execute(query, values)
