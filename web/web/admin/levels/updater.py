23a1c0ec99649b972b54020fef8a565045be4117
EmoonX <emoon@emoon.dev>
2025-05-28 01:12:58 -0300

    web.admin.levels: `LevelUpdater`, epilogue adjustments

--- web/web/admin/levels/updater.py
+++ web/web/admin/levels/updater.py
@@ -56,16 +56,12 @@ class LevelUpdater:
             # Do not allow base class initialization
             raise NotImplementedError
 
-        self.level_name = level_name
-        self.set_name = set_name
         if not level_name:
             # Levelless update
             self.level = self.level_set = None
             return
 
-        self._find_level_and_set()
-        if self.level_set:
-            self.set_name = self.level_set['name']
+        self._retrieve_level_and_set(level_name, set_name)
 
         if not self.level and not cls.current_set and not set_name:
             # Missing set name for _new AND topmost_ level entry
@@ -80,19 +76,21 @@ class LevelUpdater:
                 f"'{self.level['level_set']}' (current) and '{set_name}' (given)"
             )
 
+        self._level_name = level_name
+        self._set_name = set_name or self.level_set['name']
         self.paths = paths
 
-    def _find_level_and_set(self):
-        '''Search for (possibly existing) level and/or level set.'''
+    def _retrieve_level_and_set(self, level_name: str, set_name: str):
+        '''Search for and retrieve (possibly existing) level and/or level set.'''
         self.level = None
-        self.level_set = self.__class__.current_set
+        self.level_set = self.__class__.current_set if not set_name else None
         for level_set in self.all_levels_by_set.values():
             for level in level_set['levels'].values():
-                if level['name'] == self.level_name:
+                if level['name'] == level_name:
                     self.level = level
                     self.level_set = level_set
                     return
-            if level_set['name'] == self.set_name:
+            if level_set['name'] == set_name:
                 self.level_set = level_set
 
     async def process_level(self):
@@ -101,16 +99,16 @@ class LevelUpdater:
         if not self.level:
             if not self.level_set:
                 self.log(
-                    f"Creating new level set \033[1m{self.set_name}\033[0m…"
+                    f"Creating new level set \033[1m{self._set_name}\033[0m…"
                 )
                 await self._add_new_level_set()
             self.log(
-                f"Adding level \033[1m{self.level_name}\033[0m to the database…"
+                f"Adding level \033[1m{self._level_name}\033[0m to the database…"
             )
             await self._add_new_level()
         else:
             self.log(
-                f"Level \033[1m{self.level_name}\033[0m found in the database."
+                f"Level \033[1m{self._level_name}\033[0m found in the database."
             )
 
         if previous_level := self._get_previous_level():
@@ -132,7 +130,7 @@ class LevelUpdater:
         values = {
             'riddle': self.alias,
             'index': set_index,
-            'name': self.set_name,
+            'name': self._set_name,
         }
         await database.execute(query, values)        
         self.level_set = values | {'levels': {}}
@@ -153,12 +151,12 @@ class LevelUpdater:
         values = {
             'riddle': self.alias,
             'set_index': self.level_set['index'],
-            'level_set': self.set_name,
+            'level_set': self._set_name,
             'index': index,
-            'name': self.level_name,
+            'name': self._level_name,
             'path': self.paths[0] if self.paths else None,
-            'discord_category': self.set_name,
-            'discord_name': self.level_name.lower().replace(' ', '-'),
+            'discord_category': self._set_name,
+            'discord_name': self._level_name.lower().replace(' ', '-'),
         }
         await database.execute(query, values)
         self.level_set['levels'][index] = self.level = values
