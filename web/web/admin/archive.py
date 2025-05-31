f645ef8d1fefd5d89563cde8df96ac96e097fa47
EmoonX <emoon@emoon.dev>
2025-05-30 23:50:16 -0300

    web.admin.health: add new `showNewInDays=X` query option

--- web/web/admin/archive.py
+++ web/web/admin/archive.py
@@ -2,65 +2,135 @@ from datetime import datetime
 import hashlib
 import os
 from pathlib import Path
-
-from pymysql.err import IntegrityError
+from typing import overload, Self
 
 from util.db import database
 
 
-class ArchivePage:
+class ArchivedPage:
 
+    @classmethod
+    async def get_latest_snapshot(cls, alias: str, path: str) -> Self | None:
+        '''Retrieve latest archived page snapshot, based on retrieval time.'''
+        query = '''
+            SELECT * FROM _page_hashes
+            WHERE riddle = :riddle AND path = :path
+            ORDER BY retrieval_time DESC
+            LIMIT 1
+        '''
+        values = {'riddle': alias, 'path': path}
+        latest_snapshot = await database.fetch_one(query, values)
+        if not latest_snapshot:
+            return None        
+        return cls(alias, path, content_hash=latest_snapshot['content_hash'])
+
+    @overload
     def __init__(self, alias: str, path: str, content: bytes):
+        ...
+
+    @overload
+    def __init__(self, alias: str, path: str, content_hash: str):
+        ...
+
+    def __init__(
+        self,
+        alias: str,
+        path: str,
+        content: bytes | None = None,
+        content_hash: str | None = None,
+    ):
         self.alias = alias
         self.path = path
-        self.content = content
-        self.content_hash = hashlib.md5(content).hexdigest()
+        self.local_path = self._get_local_path(path)
+        if content:
+            self.content = content
+            self.content_hash = hashlib.md5(content).hexdigest()
+        elif content_hash:
+            self.content = self._read_local_file()
+            self.content_hash = content_hash
+            self.retrieval_time = self._get_local_mtime()
+        else:
+            raise TypeError
+
+    def _get_local_path(self, path: str) -> Path:
+        '''Ǵet full sanitized path to the (currently or to-be) archived page.'''
+        base_path = path.replace('..', '(parent)')
+        return Path(f"../archive/{self.alias}{base_path}")
 
     async def record_hash(self) -> bool:
+        '''Record possibly new content hash, return whether successful.'''
+
         query = '''
-            INSERT INTO _page_hashes
-                (riddle, path, content_hash)
-            VALUES (:riddle, :path, :content_hash)
+            SELECT retrieval_time FROM _page_hashes
+            WHERE riddle = :riddle
+                AND path = :path
+                AND content_hash = :content_hash
         '''
         values = {
             'riddle': self.alias,
             'path': self.path,
             'content_hash': self.content_hash,
         }
-        try:
-            await database.execute(query, values)
-        except IntegrityError:
+        if retrieval_time := await database.fetch_val(query, values):
+            self.retrieval_time = retrieval_time
             return False
+
+        self.retrieval_time = datetime.utcnow()
+        query = '''
+            INSERT INTO _page_hashes
+                (riddle, path, content_hash, retrieval_time)
+            VALUES (:riddle, :path, :content_hash, :retrieval_time)
+        '''
+        values |= {'retrieval_time': self.retrieval_time}
+        await database.execute(query, values)
 
         return True
 
     def save(self):
-        base_path = self.path.replace('..', '(parent)')
-        full_path = Path(f"../archive/{self.alias}{base_path}")
-        if full_path.is_file():
-            # If the currently archived file is indeed a different one,
-            # rename it as hidden and concatenate its datetime
-            with open(full_path, 'rb') as old_file:
-                old_content = old_file.read()
-                old_hash = hashlib.md5(old_content).hexdigest()
-                if old_hash != self.content_hash:
-                    old_mtime = (
-                        datetime.fromtimestamp(full_path.stat().st_mtime)
-                        .strftime("%Y-%m-%d")
-                    )
-                    renamed_filename = f".{full_path.name}-{old_mtime}"
-                    renamed_path = f"{full_path.parent}/{renamed_filename}"
-                    os.rename(full_path, renamed_path)
+        '''Save new content retrieved page as part of the archive.'''
+        local_content = self._read_local_file()
+        if local_content:
+            # Old content already exists
+            local_hash = hashlib.md5(local_content).hexdigest()
+            if local_hash != self.content_hash:
+                # Retrieved content is indeed new, so rename old one
+                self._rename_file_as_hidden()
+        self._save_local_file()
 
-        # Recursively create directories (if needed) and save new archived file
-        Path(full_path.parent).mkdir(parents=True, exist_ok=True)
-        with open(full_path, 'wb') as file:
+    def _read_local_file(self) -> bytes | None:
+        '''
+        Read the currently stored local file.
+        Return its contents as raw data, or `None` if nonexisting.
+        '''
+        try:
+            with open(self.local_path, 'rb') as file:
+                return file.read()
+        except FileNotFoundError:
+            return None
+
+    def _save_local_file(self):
+        '''Save new local file, creating intermediate directories as needed.'''
+        Path(self.local_path.parent).mkdir(parents=True, exist_ok=True)
+        with open(self.local_path, 'wb') as file:
             file.write(self.content)
-            tnow = datetime.utcnow()
             print(
                 f"> \033[1m[{self.alias}]\033[0m "
-                    f"Saved new content from \033[1;3m{self.path}\033[0m "
-                    f"with hash \033[1m{self.content_hash}\033[0m "
-                    f"({tnow})",
+                f"Saved new content from \033[1;3m{self.path}\033[0m "
+                f"with hash \033[1m{self.content_hash}\033[0m "
+                f"({self.retrieval_time})",
                 flush=True
             )
+
+    def _rename_file_as_hidden(self):
+        '''
+        Rename local file as hidden (i.e starting with '.'),
+        appending its modification date to the filename.
+        '''
+        local_mtime = self._get_local_mtime().strftime("%Y-%m-%d")
+        renamed_filename = f".{self.local_path.name}-{local_mtime}"
+        renamed_path = f"{self.local_path.parent}/{renamed_filename}"
+        os.rename(self.local_path, renamed_path)
+
+    def _get_local_mtime(self) -> datetime:
+        '''Get current local file's modification/retrieval time.'''
+        return datetime.fromtimestamp(self.local_path.stat().st_mtime)
--- web/web/admin/health.py
+++ web/web/admin/health.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-from itertools import dropwhile
+from datetime import datetime
 import re
 from urllib.parse import urljoin
 import warnings
@@ -8,10 +7,9 @@ from bs4 import BeautifulSoup, MarkupResemblesLocatorWarning
 from quart import Blueprint, render_template, request
 from quartcord import requires_authorization
 import requests
-from requests.auth import HTTPBasicAuth
 
 from admin.admin_auth import admin_auth
-from admin.archive import ArchivePage
+from admin.archive import ArchivedPage
 from credentials import get_path_credentials
 from inject import get_riddle
 from levels import absolute_paths, get_pages
@@ -50,6 +48,7 @@ async def health_diagnostics(alias: str):
         admin=True,
     )
     redo_existing = bool(args.get('redoExisting'))
+    show_new_in_days = int(args.get('showNewInDays', 0))
     start_level = args.get('start')
     end_level = args.get('end')
 
@@ -69,6 +68,16 @@ async def health_diagnostics(alias: str):
             password = credentials['password']
             url = url.replace('://', f"://{username}:{password}@")
         page_data |= {'url': url}
+
+        if show_new_in_days:
+            # Mark page as *new* if existing and newer than the X days given
+            archived_page = await ArchivedPage.get_latest_snapshot(alias, path)
+            if archived_page:
+                page_data['content_hash'] = archived_page.content_hash
+                page_data['retrieval_time'] = archived_page.retrieval_time
+                tdiff = datetime.utcnow() - archived_page.retrieval_time
+                if tdiff.days < show_new_in_days:
+                    page_data['new'] = True
 
         if not include_level:
             return page_data
@@ -96,10 +105,12 @@ async def health_diagnostics(alias: str):
         }
         if res.ok:
             # Valid page, so archive it if new/changed
-            archive_page = ArchivePage(alias, path, res.content)
-            if await archive_page.record_hash():
-                archive_page.save()
-                page_data['content_hash'] = archive_page.content_hash
+            archived_page = ArchivedPage(alias, path, res.content)
+            if await archived_page.record_hash():
+                archived_page.save()
+                page_data['new'] = True
+            page_data['content_hash'] = archived_page.content_hash
+            page_data['retrieval_time'] = archived_page.retrieval_time
 
             page_extension = path.partition('.')[-1].lower()
             if page_extension in ['htm', 'html', 'php', '']:
