0fb0bddb729dc35ff67d2d055d9f6fd621ea04d9
EmoonX <emoon@emoon.dev>
2026-04-04 23:34:48 -0300

    web.admin.auth: update `root_auth`'s guild

--- web/web/admin/admin_auth.py
+++ web/web/admin/admin_auth.py
@@ -11,11 +11,9 @@ from util.db import database
 async def root_auth():
     '''Assert you are really important.'''
     user = await discord.get_user()
-    if user.id == 315940379553955844:
-        return
     ok = await bot_request(
         'is-member-and-has-permissions',
-        guild_id=859797827554770955,  # Wonderland
+        guild_id=987832530826833920,  # Riddler's HQ
         username=user.name,
     )
     if ok != 'True':
